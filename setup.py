e8b36f8b9ee6836512fb47dc16437f38ae35d379
Daniil Galiev <liinda@yandex-team.ru>
2020-04-20 09:43:27 +0500

    up package version

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/yavia/django-namedtuples',
-    download_url='https://github.com/yavia/django-namedtuples/archive/1.0.0.tar.gz',
+    download_url='https://github.com/yavia/django-namedtuples/archive/1.1.0.tar.gz',
 
     license='GPLv3',
 
